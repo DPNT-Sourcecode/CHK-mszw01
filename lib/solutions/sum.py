2463ca4628ee3f8bcabf76a1886929b20908ffd8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-27 19:56:40 +0000

    Tue Feb 27 19:56:40 GMT 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,18 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def sum(x, y):
-    raise NotImplementedError()
+
+import unittest
+
+
+class Sum:
+
+    def sum(self, x, y):
+        return x + y
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        sum_object = Sum()
+        self.assertEqual(sum_object.sum(5,5), 10)
+        self.assertEqual(sum_object.sum(5, 10), 15)
+        self.assertEqual(sum_object.sum(0, 20), 20)
