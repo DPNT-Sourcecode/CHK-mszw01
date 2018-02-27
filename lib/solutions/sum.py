571589c0c13bed9ae27bb6d1144ef6fb4794268d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-27 19:59:06 +0000

    Tue Feb 27 19:59:06 GMT 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,18 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 
-import unittest
 
+def sum(x, y):
+    return x + y
 
-class Sum:
-
-    def sum(self, x, y):
-        return x + y
-
-
-class TestSum(unittest.TestCase):
-
-    def test_sum(self):
-        sum_object = Sum()
-        self.assertEqual(sum_object.sum(5,5), 10)
-        self.assertEqual(sum_object.sum(5, 10), 15)
-        self.assertEqual(sum_object.sum(0, 20), 20)
